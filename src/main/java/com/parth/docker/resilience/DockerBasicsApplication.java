35cccd0ed2a3d26a32b4ec34100b86d252e6597c
Parth Gupta <parth.gupta@sap.com>
2019-12-24 16:25:07 +0530

    Changing artifact id

--- src/main/java/com/parth/docker/resilience/DockerBasicsApplication.java
+++ src/main/java/com/parth/docker/resilience/DockerBasicsApplication.java
@@ -1,4 +1,4 @@
-package com.parth.learning.dockerbasics;
+package com.parth.docker.resilience;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
